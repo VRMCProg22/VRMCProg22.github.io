370f1489d5dcc2cc019dff53de4cf741e9a6b0e7
VRMCProg22 <118323148+VRMCProg22@users.noreply.github.com>
2022-11-20 15:08:25 +0800

    Update input.js

--- input.js
+++ input.js
@@ -1,23 +1,18 @@
-let btnAdd = document.querySelector('button');
-let table = document.querySelector('table');
 
 
-let nameInput = document.querySelector('#name');
-let ageInput = document.querySelector('#age');
-let countryInput = document.querySelector('#country');
 
 
-btnAdd.addEventListener('click', () => {
-    let name = nameInput.value;
-    let age = ageInput.value;
-    let country = countryInput.value;
 
-    let template = `
-                <tr>
-                    <td>${name}</td>
-                    <td>${age}</td>
-                    <td>${country}</td>
-                </tr>`;
+let name = 'e';
+let age = 's';
+let country = 't';
 
-    table.innerHTML += template;
-});
+let template = `
+            <tr>
+                <td>${name}</td>
+                <td>${age}</td>
+                <td>${country}</td>
+            </tr>`;
+
+table.innerHTML += template;
+
